b6c78d9abd13de3d179c357eca174fcfbeff8ef9
Mike <mmrrff2099@gmail.com>
2026-01-20 15:34:56 +0200

    Day_1

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './css/style.css'
+import './css/font-awesome5.css'
+import App from './App.vue'
+import router from './router/router'
+
+createApp(App).use(router).mount('#app')
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,38 @@
+<script setup>
+import "../css/login.css"
+</script>
+
+<template>
+    <div class="login">
+        <div class="right-area">
+            <img src="../assets/Login_Image.png" alt="Login Image">
+        </div>
+        <div class="left-area">
+            <div class="header">
+                <div class="wrapper">
+                    <div class="ar">
+                        <h1>Affiliate Sign in</h1>
+                    </div>
+                    <div class="logo">
+                        <img src="../assets/logo.png" alt="Logo">
+                    </div>
+                </div>
+            </div>
+            <div class="form">
+                <form class="form-login">
+                    <div class="row">
+                        <label>Email</label>
+                        <input type="email" required />
+                    </div>
+                    <div class="row">
+                        <label>Password</label>
+                        <input type="password" required autocomplete="on" />
+                    </div>
+                    <div class="row">
+                        <button class="btn">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
